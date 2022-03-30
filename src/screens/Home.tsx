2e71cc8236d53ca5981b7deaaec8292d6727057e
DevHola <56859027+DevHola@users.noreply.github.com>
2022-03-30 10:24:07 -0700

    corespond
    corespond

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -401,11 +401,6 @@ function Home() {
                           Balance: {balance / 1000000} Algos{" "}
                         </h3>
                       )}
-                      {balance === 0 && (
-                        <h3 className="text-white">
-                          You currently have no Algorand in Your Wallet
-                        </h3>
-                      )}
                     </div>
                   </div>
                 )}
